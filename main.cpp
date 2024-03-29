0a063f70002af18093f76a11798a50d718d3364e
MunyinSam <kelvinsam233@gmail.com>
2024-03-29 23:43:48 +0700

    add cpp file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I like pizza!" << std::endl; // standard char out
+    std::cout << "It's really good" << '\n';
+    return 0; // if 1 is return theres a problem
+}

91c9cadba77c539b332eae5562ef30d7c52bf9ef
MunyinSam <kelvinsam233@gmail.com>
2024-03-31 00:32:27 +0700

    c

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
+
 int main(){
+
+    int x;
+    x = 123;
+    int y = 3322;
+
+    int age = 21;
+    float gpa = 2.06;
+    char grade = 'C';
+    char name[] = "Yooo"; //Array
+
+    printf("You are  %d noob\n", x);
+    printf("Hello %s\n",name);
+    printf("%c", grade);
+    printf("%f", )
+
+
     printf("I \tlike pizza!\n");
     return 0;
 }
